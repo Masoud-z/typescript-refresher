a631a4d543773ca43434ae077ed17ee74af7db42
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-13 16:27:19 +0330

    TypeScript object type

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 4;
-var number2 = 12;
-var printResult = true;
-var resultPrase = "Result is: ";
-add(number1, number2, printResult, resultPrase);
+var person = {
+    name: "Masoud",
+    age: 27,
+};
+console.log(person.name);
