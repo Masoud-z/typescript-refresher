7025d72f06ca46fa34619b9ae2a926c34204a4f4
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-13 17:47:15 +0330

    TypeScript Type Aliases

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
+type Combinator = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
+  input1: Combinator,
+  input2: Combinator,
+  resultConversion: ConversionDescriptor
 ) {
   let result: number | string;
   if (
