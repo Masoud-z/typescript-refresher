fbde60fb575f5a5e218e61cc1f51521dbfe91641
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-13 16:34:05 +0330

    TypeScript Array type

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var person = {
     name: "Masoud",
     age: 27,
+    hubbies: ["Sports", "Movies"],
 };
+var favoriteActivities;
+favoriteActivities = [
+    "Playing Video Games",
+    "Hanging out with friends",
+    "Netflix and chill",
+];
 console.log(person.name);
+for (var _i = 0, _a = person.hubbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,18 @@
-const person: { name: string; age: number } = {
+const person: { name: string; age: number; hubbies: string[] } = {
   name: "Masoud",
   age: 27,
+  hubbies: ["Sports", "Movies"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = [
+  "Playing Video Games",
+  "Hanging out with friends",
+  "Netflix and chill",
+];
+
 console.log(person.name);
+
+for (const hobby of person.hubbies) {
+  console.log(hobby);
+}
