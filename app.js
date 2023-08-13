f15f84c4286de98f56e99b820c0c51ac52620703
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-13 18:02:24 +0330

    TypeScript function return types

--- app.js
+++ app.js
@@ -1,17 +1,6 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedNumbers = combine(20, 30, "as-number");
-console.log(combinedNumbers);
-var combinedStringNumbers = combine("20", "30", "as-number");
-console.log(combinedStringNumbers);
-var combinedNames = combine("Masoud", "Ghazal", "as-text");
-console.log(combinedNames);
+function printResult(num) {
+    console.log(num);
+}
