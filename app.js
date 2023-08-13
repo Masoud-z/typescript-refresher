b01df39a2a6c3e7f2d30f77a29764f3d4240b516
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-13 17:43:04 +0330

    TypeScript Literal types

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedNumbers = combine(20, 30);
+var combinedNumbers = combine(20, 30, "as-number");
 console.log(combinedNumbers);
-var combinedNames = combine("Masoud", "Ghazal");
+var combinedStringNumbers = combine("20", "30", "as-number");
+console.log(combinedStringNumbers);
+var combinedNames = combine("Masoud", "Ghazal", "as-text");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,25 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result: number | string;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combinedNumbers = combine(20, 30);
+const combinedNumbers = combine(20, 30, "as-number");
 console.log(combinedNumbers);
 
-const combinedNames = combine("Masoud", "Ghazal");
+const combinedStringNumbers = combine("20", "30", "as-number");
+console.log(combinedStringNumbers);
+
+const combinedNames = combine("Masoud", "Ghazal", "as-text");
 console.log(combinedNames);
