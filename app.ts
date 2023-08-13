b3b71bcb2c6fe85b7b9c683ed8a1318e229cafeb
Masoud <masoud.zeinalabedini@gmail.com>
2023-08-13 15:31:43 +0330

    TypeScript number type

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Project started");
+function add(number1: number, number2: number) {
+  return number1 + number2;
+}
+
+const number1 = 4;
+const number2 = 12;
+
+const result = add(number1, number2);
+
+console.log(result);
